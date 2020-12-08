50662b735a1a818a163a73fcdea08beac4ba00f4
Amaya Kennedy <43918092+Budderprincess14@users.noreply.github.com>
2020-12-07 20:28:15 -0500

    Added scripts

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+//Making the button for the github repo link
+const buttonRepo = document.querySelector(".repo");
+
+buttonRepo.onclick = redirectRepo();
+function redirectRepo() {
+     window.location.href = "https://github.com/Budderprincess14/Hello-Student-Council";
+};
+
+
+//making the button for the surprise
+const buttonSurprise = document.querySelector(".surprise");
+
+buttonSurprise.onclick = redirectSuprise();
+function redirectSuprise() {
+    window.location.href = "";
+};
+
+
